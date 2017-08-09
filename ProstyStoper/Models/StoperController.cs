39e13079479e64a3a891723d9d4849aa5994a1e2
kozen <kozen@DESKTOP-TG7VLQH>
2017-08-09 16:47:08 +0300

    Add project files.

---
+++ ProstyStoper/Models/StoperController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ProstyStoper.Models
+{
+    public class StoperController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
